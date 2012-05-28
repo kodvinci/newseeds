77ee92c76603a15977f42c2ceae230cdd8ed70ff
Leonard K. Ng'eno <dranoel.bravo@gmail.com>
2012-05-28 17:52:47 +0300

    more frustrations

--- db/migrate/20120524122052_editlenders.rb
+++
@@ -1,5 +0,0 @@
-class Editlenders < ActiveRecord::Migration
-  def change
-	  remove_column :lenders, :credit
-  end
-end
--- db/migrate/20120524122651_add_creditto_lenders.rb
+++
@@ -1,6 +0,0 @@
-class AddCredittoLenders < ActiveRecord::Migration
-  def change
-	  add_column :lenders, :credit, :integer, :default => 0
-  end
-
-end
