91e5a8498645c9f4a675ec1050f9ed7f50168afe
Leonard K. Ng'eno <dranoel.bravo@gmail.com>
2012-05-30 11:46:12 +0300

    paypal API credentials

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -2,7 +2,7 @@ class PurchaseController < ApplicationController
   before_filter :require_ssl, :only => [ :index, :credit ]
   before_filter :load_card
   
-  BILL_AMOUNT = 1200
+  BILL_AMOUNT = 500
   
   def index
   end
